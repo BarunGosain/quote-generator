6f7d0a9051eb7c519a7fd1b7754136bfc6be0566
BarunGosain <barun.22.010@gmail.com>
2023-04-26 20:02:23 +0530

    Implemented loader and error handling

--- script.js
+++ script.js
@@ -2,10 +2,27 @@ const quoteText = document.getElementById("quote-text");
 const quoteAuther = document.getElementById("quote-auther");
 const qouteTwitterBtn = document.getElementById("quote-twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const quoteContainer = document.getElementById("quote-container");
+const loader = document.getElementById("loader");
+const notFound = document.getElementById("not-found");
 
 let apiQoutes = [];
+let tryCount = 0;
 
-function newQuote(){;
+function showLoaderSpinner(){
+    quoteContainer.hidden = true;
+    loader.hidden = false;
+}
+
+function hideLoaderSpinner(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
+
+
+function newQuote(){
+    showLoaderSpinner();
     const quote = apiQoutes[Math.floor(Math.random() * apiQoutes.length)];
     quoteText.textContent = quote.text;
     if(quote.text.length > 50){
@@ -19,18 +36,33 @@ function newQuote(){;
     }else{
         quoteAuther.textContent = quote.author;
     }
+
+    hideLoaderSpinner();
 }
 
 async function getQuotes(){
+    quoteContainer.hidden = true;
+    notFound.hidden = true;
     const apiUrl = "https://type.fit/api/quotes";
 
     try{
         const response = await fetch(apiUrl);
         apiQoutes = await response.json();
+        showLoaderSpinner();
         newQuote();
     }
     catch(error){
+        if(tryCount < 10){
+            console.log("tryCount : ",tryCount);
+            getQuotes();
+        }
+        tryCount++;
 
+        if(tryCount > 4){
+            loader.hidden = true;
+            quoteContainer.hidden = true;
+            notFound.hidden = false;
+        }
     }
 }
 
